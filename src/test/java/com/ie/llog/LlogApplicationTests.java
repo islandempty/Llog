971bb697e8df98921e559235b85bdac535271d74
islandempty <islandempty@foxmail.com>
2023-02-14 23:15:37 +0800

    init

---
+++ src/test/java/com/ie/llog/LlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ie.llog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
